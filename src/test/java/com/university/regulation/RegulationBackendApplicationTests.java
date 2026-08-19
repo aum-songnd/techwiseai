128864201799c89a22015792d6c42a6d569a3c0f
quan <aum.quanph@gmailcom>
2026-08-19 13:57:27 +0700

    chore: initialize Spring Boot backend

---
+++ src/test/java/com/university/regulation/RegulationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.university.regulation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegulationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
