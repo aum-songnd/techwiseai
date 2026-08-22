0a5cac1212db12c0dc35deeeb8cbeb535fd54390
songnd <aum.songnd@gmail.com>
2026-08-22 15:13:31 +0700

    cm

--- components/HomeBanner.tsx
+++ components/HomeBanner.tsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const HomeBanner = () => {
   return (
-    <div className='py-16 md:py-0 bg-shop_light_pink rounded-lg px-10 lg:px-24 flex items-center  justify-between'>HomeBanner</div>
+    <div className='py-16 md:py-0 bg-shop_light_pink rounded-lg px-10 lg:px-24 flex items-center  justify-between'>
+        <div>
+            
+        </div>
+        <div></div>
+    </div>
   )
 }
 
